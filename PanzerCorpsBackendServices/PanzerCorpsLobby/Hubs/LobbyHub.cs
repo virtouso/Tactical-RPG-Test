ddf535b1d81e6025277de880c0ec2ca6b4c2db31
LAPTOP-9KQD6BO4\s_moe <s.moeen.kamali@gmail.com>
2021-12-05 01:22:31 +0330

    initiated signalr

--- PanzerCorpsBackendServices/PanzerCorpsLobby/Hubs/LobbyHub.cs
+++ PanzerCorpsBackendServices/PanzerCorpsLobby/Hubs/LobbyHub.cs
@@ -1,6 +1,14 @@
-﻿namespace PanzerCorpsLobby.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PanzerCorpsLobby.Hubs
 {
-    public class LobbyHub
+    public class LobbyHub:Hub
     {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+
     }
 }
--- PanzerCorpsBackendServices/PanzerCorpsLobby/Program.cs
+++ PanzerCorpsBackendServices/PanzerCorpsLobby/Program.cs
@@ -1,15 +1,21 @@
+
+
+using PanzerCorpsLobby.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -17,5 +23,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapHub<LobbyHub>("/LobbyHub");
 
 app.Run();
